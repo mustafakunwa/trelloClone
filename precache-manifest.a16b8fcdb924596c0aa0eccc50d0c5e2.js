7cefe8293f8cee578c6db9b459df59639055b078
Mustafa kunwa <m.kunwaw52@gmail.com>
2022-05-14 15:26:31 +0530

    Updates

---
+++ precache-manifest.a16b8fcdb924596c0aa0eccc50d0c5e2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3ecfbb5f87a81d4a54a99a505e369b2",
+    "url": "/trelloClone/index.html"
+  },
+  {
+    "revision": "dd9e37a37b24f5b7c85a",
+    "url": "/trelloClone/static/js/2.0a002466.chunk.js"
+  },
+  {
+    "revision": "89a1b2dcd30c03705b2bceeb141b76b6",
+    "url": "/trelloClone/static/js/2.0a002466.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dfba81e3acca584f4fb8",
+    "url": "/trelloClone/static/js/main.77b9bfeb.chunk.js"
+  },
+  {
+    "revision": "27924afd01ac938d6af0",
+    "url": "/trelloClone/static/js/runtime-main.95e6ff99.js"
+  }
+]);
